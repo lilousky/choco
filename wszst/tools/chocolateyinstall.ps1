deb55e068a6ea04e45df125da6c314de33219261
Lami <58743698+Lami-Lemmy@users.noreply.github.com>
2022-05-21 10:43:00 -0400

    update right away

--- wszst/tools/chocolateyinstall.ps1
+++ wszst/tools/chocolateyinstall.ps1
@@ -1,15 +1,17 @@
-﻿$url_version = 'v1.58b-r7503'
+﻿$url_version = 'v2.27a-r8529'
+$os_bitness = Get-OSBitness
 
 $ErrorActionPreference = 'Stop'; 
 $toolsDir  	 	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        	= 'https://szs.wiimm.de/download/szs-'+$url_version+'-cygwin.zip'
+$url        	= 'https://szs.wiimm.de/download/szs-'+$url_version+'-cygwin'+$os_bitness+'.zip'
 $packageName	= $env:ChocolateyPackageName
   
-Get-ChocolateyWebFile $packageName "$($toolsDir)\tmp\szs-$($url_version)-cygwin.zip" $url 
-Get-ChocolateyUnzip -FileFullPath "$($toolsDir)\tmp\szs-$($url_version)-cygwin.zip" -Destination "$($toolsDir)" -SpecificFolder "szs-$($url_version)-cygwin\bin\*" -PackageName 'wszst'
+Get-ChocolateyWebFile $packageName "$($toolsDir)\tmp\szs-$($url_version)-cygwin$($os_bitness)" $url 
+Get-ChocolateyUnzip -FileFullPath "$($toolsDir)\tmp\szs-$($url_version)-cygwin$($os_bitness).zip" -Destination "$($toolsDir)" -SpecificFolder "szs-$($url_version)-cygwin$($os_bitness)\bin\*" -PackageName 'wszst'
 
-Move-Item "$($toolsDir)\szs-$($url_version)-cygwin\bin\*" "$($toolsDir)\"
-Remove-Item -Recurse -Force "$($toolsDir)\szs-$($url_version)-cygwin\"
+Move-Item "$($toolsDir)\szs-$($url_version)-cygwin$($os_bitness)\bin\*" "$($toolsDir)\"
+Remove-Item -Recurse -Force "$($toolsDir)\szs-$($url_version)-cygwin$($os_bitness)\"
 Remove-Item -Recurse -Force "$($toolsDir)\tmp\"
 
 $null > "$($toolsDir)\windows-install.exe.ignore"
+$null > "$($toolsDir)\windows-uninstall.exe.ignore"
