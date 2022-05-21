867a7d3072db9ef19aad3ad06494be37b3bd0ad1
Lami <58743698+Lami-Lemmy@users.noreply.github.com>
2022-05-21 10:23:45 -0400

    Push update

--- wit/tools/chocolateyinstall.ps1
+++ wit/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $toolsDir  	 	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        	= 'https://wit.wiimm.de/download/wit-'+$url_version+'-cygwin'+$os_bitness+'.zip'
 $packageName	= $env:ChocolateyPackageName
   
-Get-ChocolateyWebFile $packageName "$($toolsDir)\tmp\wit-$($url_version)-cygwin32.zip" $url 
-Get-ChocolateyUnzip -FileFullPath "$($toolsDir)\tmp\wit-$($url_version)-cygwin32.zip" -Destination "$($toolsDir)" -SpecificFolder "wit-$($url_version)-cygwin32\bin\*" -PackageName 'wit'
+Get-ChocolateyWebFile $packageName "$($toolsDir)\tmp\wit-$($url_version)-cygwin$($os_bitness).zip" $url 
+Get-ChocolateyUnzip -FileFullPath "$($toolsDir)\tmp\wit-$($url_version)-cygwin$($os_bitness).zip" -Destination "$($toolsDir)" -SpecificFolder "wit-$($url_version)-cygwin$($os_bitness)\bin\*" -PackageName 'wit'
 
-Move-Item "$($toolsDir)\wit-$($url_version)-cygwin32\bin\*" "$($toolsDir)\"
-Remove-Item -Recurse -Force "$($toolsDir)\wit-$($url_version)-cygwin32\"
+Move-Item "$($toolsDir)\wit-$($url_version)-cygwin$($os_bitness)\bin\*" "$($toolsDir)\"
+Remove-Item -Recurse -Force "$($toolsDir)\wit-$($url_version)-cygwin$($os_bitness)\"
 Remove-Item -Recurse -Force "$($toolsDir)\tmp\"
 
 $null > "$($toolsDir)\windows-install.exe.ignore"
